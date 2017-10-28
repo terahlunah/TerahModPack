e0b2bdbb9d674987842846f300fcf51c0112c5ee
Terah <terah.dev@gmail.com>
2017-10-28 13:07:18 +0200

    First commit, contains NoFog + Flatlands

---
+++ Flatlands/FlatlandsMod.cs
@@ -0,0 +1,40 @@
+﻿using Merlin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Flatlands
+{
+    public class FlatlandsMod : MerlinMod
+    {
+        public override void OnGenerateFertileTiles(TerrainGen terrain)
+        {
+            for (int x = 0; x < World.inst.GridWidth; x++)
+            {
+                for (int y = 0; y < World.inst.GridHeight; y++)
+                {
+                    terrain.SetFertileTile(x, y, 2);
+                }
+            }
+        }
+
+        public override void OnGenerateWater(TerrainGen terrain)
+        {
+            for (int x = 0; x < World.inst.GridWidth; x++)
+            {
+                for (int y = 0; y < World.inst.GridHeight; y++)
+                {
+                    terrain.SetLandTile(x, y);
+                    Cell cell = World.inst.GetCellData(x, y);
+                    cell.Type = ResourceType.None;
+                }
+            }
+        }
+
+        public override void OnGenerateTrees(TreeGrowth treeGrowth)
+        {
+            treeGrowth.MaxTreesOnMap = 3200;
+        }
+    }
+}
---
+++ NoFog/NoFogMod.cs
@@ -0,0 +1,15 @@
+﻿using Merlin;
+using UnityEngine;
+
+using Merlin.Extensions;
+
+namespace NoFog
+{
+    public class NoFogMod : MerlinMod
+    {
+        public override void OnLoad()
+        {
+            Weather.DisableFog();
+        }
+    }
+}
